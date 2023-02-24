eb17658e5e4096adaa2e06369f52e9859e3dfb1e
Daxit godhani <dakshitgodhani103@gmail.com>
2023-02-24 19:25:52 +0530

    html change

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    <div style={{height:"50px"}}>
-      {props.alert &&<div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{props.alert.type}</strong>: {props.alert.msg}
-        </div>}
+    <div style={{ height: "50px" }}>
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Alert :</strong>{props.msg}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
     </div>
   )
 }
